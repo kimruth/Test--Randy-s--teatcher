1bb45769c7c180b94a0fba41022f3203b39f2603
kimruth <seugnouruth21@gmail.com>
2023-09-19 04:08:37 +0100

    drop down menu: html, css, js

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+let presentation = document.getElementsByClassName('.presentation');
+let equipe = document.getElementsByClassName('.equipe');
+let recherches = document.getElementsByClassName('.recherches');
+let programmes = document.getElementsByClassName('.programmes');
+let publications = document.getElementsByClassName('.publications');
+let formations = document.getElementsByClassName('.formations')
+let firstDrop = document.getElementsByClassName('.first-drop');
+let secondDrop = document.getElementsByClassName('.second-drop');
+let thirdDrop = document.getElementsByClassName('.third-drop');
+let forthDrop = document.getElementsByClassName('.forth-drop');
+let fifthDrop = document.getElementsByClassName('.fith-drop');
+let sixthDrop = document.getElementsByClassName('.sixth-drop');
+
+presentation.style.display = 'none';
+equipe.style.display = 'none';
+recherches.style.display = 'none';
+programmes.style.display = 'none';
+publications.style.display = 'none';
+formations.style.display = 'none';
